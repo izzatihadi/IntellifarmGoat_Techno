d351eb93fbe8a546fdae66e688526584b321d949
izzatihadi <114452144+izzatihadi@users.noreply.github.com>
2023-11-15 00:59:19 +0800

    Upload All Files
    Upload Excel files contains all datasets of goats. Upload python script used to define the health status of goats and determine potential disease based on its symptoms. Refine latest flowchart for machine learning model.

---
+++ test.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import statsmodels.api as sm
+
+# Load Excel file
+data = pd.read_excel("C:/Users/Nor Sa'adah/Downloads/Technopreneurship/Machine Learning/.vscode/datasets.xlsx")
+print(data)
+
+# Extract the 'Temperature' and 'Heartbeat' columns from the Excel
+X = data[['Temperature', 'Heartbeat']]
+
+# Add a constant for the intercept
+X = sm.add_constant(X)
+
+# Load the coefficients for the regression model
+a = -0.4870
+b = 0.0600
+intercept = 25.1534
+
+# Calculate the 'Y' values using the regression formula
+data['Y'] = intercept + (a * data['Temperature']) + (b * data['Heartbeat']) 
+
+# Define the health status criteria
+lower_limit = 10.0195
+upper_limit = 11.2039
+
+# Calculate health status based on Y values
+data['Health Status'] = ['HEALTHY' if lower_limit <= y <= upper_limit else 'UNHEALTHY' for y in data['Y']]
+
+# Extract values of Fever, DifficultyBreathing, Fatigue, and Cough
+fever = data['Fever']
+difficulty_breathing = data['DifficultyBreathing']
+fatigue = data['Fatigue']
+cough = data['Cough']
+
+# Set conditions based on data for symptoms
+def determine_secondary_health(row):
+    if row['Fever'] == 'Y' and row['DifficultyBreathing'] == 'Y' and row['Fatigue'] == 'Y' and row['Cough'] == 'Y':
+        return 'Anthrax and Pneumonia'
+    elif row['Fever'] == 'Y' and row['DifficultyBreathing'] == 'Y' and row['Fatigue'] == 'N' and row['Cough'] == 'N':
+        return 'Anthrax or Pneumonia'
+    elif row['Fever'] == 'Y' and row['DifficultyBreathing'] == 'N' and row['Fatigue'] == 'Y' and row['Cough'] == 'Y':
+        return 'Anthrax or Pneumonia'
+    elif row['Fever'] == 'Y' and row['DifficultyBreathing'] == 'Y' and row['Fatigue'] == 'Y' and row['Cough'] == 'N':
+        return 'Anthrax'
+    elif row['Fever'] == 'N' and row['DifficultyBreathing'] == 'Y' and row['Fatigue'] == 'Y' and row['Cough'] == 'N':
+        return 'Anthrax'
+    elif row['Fever'] == 'Y' and row['DifficultyBreathing'] == 'N' and row['Fatigue'] == 'Y' and row['Cough'] == 'N':
+        return 'Anthrax'
+    elif row['Fever'] == 'N' and row['DifficultyBreathing'] == 'N' and row['Fatigue'] == 'Y' and row['Cough'] == 'N':
+        return 'Anthrax'
+    elif row['Fever'] == 'Y' and row['DifficultyBreathing'] == 'Y' and row['Fatigue'] == 'N' and row['Cough'] == 'Y':
+        return 'Pneumonia'
+    elif row['Fever'] == 'Y' or row['DifficultyBreathing'] == 'Y' and row['Fatigue'] == 'N' and row['Cough'] == 'Y':
+        return 'Pneumonia'
+    elif row['Fever'] == 'Y' and row['DifficultyBreathing'] == 'N' and row['Fatigue'] == 'N' and row['Cough'] == 'Y':
+        return 'Pneumonia'
+    elif row['Fever'] == 'N' and row['DifficultyBreathing'] == 'N' and row['Fatigue'] == 'N' and row['Cough'] == 'Y':
+        return 'Pneumonia'
+    else:
+        return ''
+
+# Determine the secondary health condition
+data['Potential Disease'] = data.apply(determine_secondary_health, axis=1)
+
+# Save the updated data with 'Y', 'Health Status' columns to a new Excel file
+data.to_excel('Resultsss.xlsx', index=False)
